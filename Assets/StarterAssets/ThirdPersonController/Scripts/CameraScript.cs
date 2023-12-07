640975765e7336417f8eb961e1f994b9ee48f8b9
UJystrik <ujystrik@mail.ru>
2023-12-07 18:49:44 +0300

    fix camera

--- Assets/StarterAssets/ThirdPersonController/Scripts/CameraScript.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/CameraScript.cs
@@ -24,7 +24,7 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Physics.Linecast(normalPosition.position, frontPosition.position, out hit))
+        if (Physics.Linecast(frontPosition.position, normalPosition.position, out hit, 3))
         {
             tempDistance = Vector3.Distance(mainCamera.transform.position, frontPosition.position) - Vector3.Distance(frontPosition.position, hit.point);
             newPosition = mainCamera.transform.localPosition + new Vector3(0, 0, +tempDistance);
